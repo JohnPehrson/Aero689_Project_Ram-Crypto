33e6cbe84030561caa71082f04b5d7a747d30b96
John C Pehrson <54759417+JohnPehrson@users.noreply.github.com>
2022-11-18 11:13:17 -0600

    Working on non-normality

--- Aero689Project_CryptoRam.R
+++ Aero689Project_CryptoRam.R
@@ -2,6 +2,7 @@ rm(list = ls())   #clear current memory
 graphics.off()    #close all graphics windows
 cat("\014")       #clear the console
 
+setwd("~/GitHub/Aero689_Project_Ram-Crypto")
 library(tidyverse)
 library(ggplot2)
 source("toDateTime.R")
@@ -12,6 +13,7 @@ library(lmerTest)
 library(ggeffects)
 library(emmeans)
 library(ggResidpanel)
+library(ggpubr)
 
 Crypto_factors_filename     = "DIM_CRYPTO_DATA_filt.csv"
 GPU_factors_filename        = "DIM_GPU_PROD_filt.csv"
@@ -31,7 +33,7 @@ Price.Crypto <- read.csv(Crypto_price_filename, header=TRUE, stringsAsFactors=FA
 
   #get all cards of the same type
     all_card_ids = Factors.GPU$Id
-    card_id = all_card_ids[5]
+    card_id = all_card_ids[12]
     single_card <- Price.GPU[Price.GPU$ProdId == card_id,]
 
   #get all prices of a single currency
@@ -42,7 +44,7 @@ Price.Crypto <- read.csv(Crypto_price_filename, header=TRUE, stringsAsFactors=FA
   #left-join the card with a single cryptocurrency
     corr_df = merge(single_card, single_curr, by = "TimeId")
       #what if I trim the data down to when crypto really skyrockets?
-    corr_df = corr_df[corr_df$TimeId>20130000,]
+    corr_df = corr_df[corr_df$TimeId>20170500,]
     corr_df = toDateTime(corr_df)
   #make factors
     names(Factors.Merchant)[1] <- 'MerchantId'
@@ -77,33 +79,56 @@ Price.Crypto <- read.csv(Crypto_price_filename, header=TRUE, stringsAsFactors=FA
   
   #fixed model
   m1.gls <- gls(Price_USD~Open, data = corr_df, method = "REML",na.action=na.omit)
-  #random intercept and random slope model
-  m1.lme4 <- lmer(Price_USD~Open+RegionId + (1|RegionId), data = corr_df, REML = TRUE)
-  m2.lme4<- lmer(Price_USD~Open+RegionId*Merchant + (1|RegionId), data = corr_df, REML = FALSE)
-  m2a.lme4<- lmer(Price_USD~Open+RegionId*Merchant + (1|RegionId), data = corr_df, REML = FALSE)
+  #alternative models
+  m1.lme4 <- lmer(Price_USD~Open + (1|RegionId), data = corr_df, REML = TRUE)
+  m1a.lme4 <- lmer(Price_USD~Open+RegionId + (1|RegionId), data = corr_df, REML = TRUE)
+  m2.lme4<- lmer(Price_USD~Open+RegionId*Merchant + (1|RegionId), data = corr_df, REML = TRUE)
+  m2a.lme4<- lmer(Price_USD~Open+RegionId+Merchant + (1|RegionId), data = corr_df, REML = TRUE)
   
   #AIC comparison of FM and alt
-  AIC(m1.gls,m1.lme4)
-  summary(m2.lme4)
-  anova(m1.lme4,m2.lme4)
+  AIC(m1.gls,m1.lme4,m1a.lme4,m2.lme4,m2a.lme4)
+  
+  #Narrow down plausible models
+  m1.lme4_ml <- lmer(Price_USD~Open + (1|RegionId), data = corr_df, REML = FALSE)
+  m1a.lme4_ml <- lmer(Price_USD~Open+RegionId + (1|RegionId), data = corr_df, REML = FALSE)
+  m2.lme4_ml<- lmer(Price_USD~Open+RegionId+Merchant + (1|RegionId), data = corr_df, REML = FALSE)
+  
+  # anova(m1.lme4_ml)
+  # anova(m1a.lme4_ml)
+  # anova(m2.lme4_ml)
+  # anova(m1.lme4_ml,m1a.lme4_ml,m2.lme4_ml)
+  #use m1, with random intercept on region
+  m1.lme4 <- lmer(Price_USD~Open + (1|RegionId), data = corr_df, REML = TRUE)
+  summary(m1.lme4)
+    
+  #various data tests
+  resid_panel(m1.lme4, plots = "all")
+  ggqqplot(resid(m1.lme4))
+  #residuals are pretty substantially  non-normal. Use a transformation
+  #try logorithmic on dependent variable (cryptocurrency price)
+  #try a square-root on dependent variable (cryptocurrency price)
+  m1.lme4_log <- lmer(Price_USD~log(Open) + (1|RegionId), data = corr_df, REML = TRUE)
+  m1.lme4_sqrt <- lmer(Price_USD~sqrt(Open) + (1|RegionId), data = corr_df, REML = TRUE)
+  summary(m1.lme4_log)
+  summary(m1.lme4_sqrt)
+  
+  resid_panel(m1.lme4_log, plots = "all")
+  resid_panel(m1.lme4_sqrt, plots = "all")
+  #still not fixed. Work to create/use a transformation to correct for data non-normality
   
   
-  
-  #linear plot of gpu vs crypto price, split by region
-  ggplot(corr_df, aes(x = Open, y = Price_USD)) +
-    geom_point() +
-    stat_smooth(method = "lm") +
-    facet_wrap(~ RegionId, ncol = 3)
   
 
-  #Mixed effect model with random intercept by region
-  m1 <- lmer(Price_USD ~ Open + (1|RegionId), data = corr_df, REML = TRUE)
-  summary(m1)
-  m1.plot <- ggemmeans(m1, c("Open"), type = "fe")
+  #Plotting the chosen fit
+  m1.plot <- ggemmeans(m1.lme4, c("Open"), type = "fe")
   ggplot(corr_df, aes(x = Open)) +
     geom_point(aes(y = Price_USD, color = RegionId)) +
     geom_line(data = m1.plot, aes(x = x, y = predicted)) +
     geom_ribbon(data = m1.plot, aes(x = x, ymin = conf.low, ymax = conf.high), alpha = 0.2)
+
+
+
+  
   
   
   
